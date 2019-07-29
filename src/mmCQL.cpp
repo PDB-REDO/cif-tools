2ac48321414a741a2bd38dca6655fcc0b89780f9
maarten <maarten@a1961a4f-ab94-4bcc-80e8-33b5a54de466>
2019-07-29 10:09:02 +0000

    a first select
    git-svn-id: svn+ssh://gitlab/srv/svn-repos/pdb-redo/trunk@451 a1961a4f-ab94-4bcc-80e8-33b5a54de466

--- src/mmCQL.cpp
+++ src/mmCQL.cpp
@@ -4,7 +4,7 @@
 #include <functional>
 
 #include <boost/program_options.hpp>
-// #include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string.hpp>
 // #include <boost/filesystem/path.hpp>
 // #include <boost/filesystem/fstream.hpp>
 // #include <boost/iostreams/device/file_descriptor.hpp>
@@ -19,12 +19,12 @@
 // #include "cif++/Cif2PDB.h"
 #include "cif++/Structure.h"
 // #include "cif++/CifParser.h"
-// #include "cif++/CifValidator.h"
+#include "cif++/CifValidator.h"
 #include "cif++/CifUtils.h"
 
 using namespace std;
 namespace po = boost::program_options;
-// namespace ba = boost::algorithm;
+namespace ba = boost::algorithm;
 namespace fs = boost::filesystem;
 // namespace io = boost::iostreams;
 namespace c = mmcif;
@@ -38,14 +38,84 @@ using unicode = uint32_t;
 using cif::iequals;
 
 class Statement;
+typedef shared_ptr<Statement> StatementPtr;
+
+// -----------------------------------------------------------------------
+
+class Statement
+{
+  public:
+	Statement(const Statement& ) = delete;
+	Statement& operator=(const Statement&) = delete;
+
+	virtual ~Statement() {}
+
+	virtual void Execute() = 0;
+
+  protected:
+	Statement() {}
+};
+
+// -----------------------------------------------------------------------
+
+class StatementList : public Statement
+{
+  public:
+	StatementList() {}
+
+	void Add(StatementPtr stmt)
+	{
+		mStatements.emplace_back(stmt);
+	}
+
+	virtual void Execute()
+	{
+		for (auto stmt: mStatements)
+			stmt->Execute();
+	}
+
+  private:
+	vector<StatementPtr> mStatements;
+};
+
+// -----------------------------------------------------------------------
+
+class SelectStatement : public Statement
+{
+  public:
+	SelectStatement(cif::Category& category, vector<string>&& items, cif::Condition&& where)
+		: mCategory(category), mItems(move(items)), mWhere(move(where)) {}
+
+	virtual void Execute()
+	{
+		vector<string> fields(mItems.size());
+
+		for (auto r: mCategory.find(move(mWhere)))
+		{
+			transform(mItems.begin(), mItems.end(), fields.begin(),
+				[r](auto item) {
+					cif::detail::ItemReference ref = r[item];
+					return ref.as<string>();
+					});
+			cout << ba::join(fields, "\t") << endl;
+		}
+	}
+
+  private:
+	cif::Category& mCategory;
+	vector<string> mItems;
+	cif::Condition mWhere;
+};
+
+// -----------------------------------------------------------------------
 
 class Parser
 {
   public:
-	Parser(const cif::Datablock& db)
+	Parser(cif::Datablock& db)
 		: mDb(db) {}
 
-	Statement* Parse(streambuf* is);
+	StatementPtr Parse(streambuf* is);
 
   private:
 
@@ -64,6 +134,13 @@ class Parser
 		semicolon,
 		asterisk,
 
+		eq_,
+		lt_,
+		le_,
+		gt_,
+		ge_,
+		ne_,
+
 		string,
 		integer,
 		number,
@@ -71,8 +148,64 @@ class Parser
 		ident,
 
 		select,
-		from
+		distinct,
+		from,
+		update,
+		set,
+		where,
+		and_,
+		or_,
+		not_,
+		insert,
+		delete_,
+		into,
+		values,
 	};
+
+	string Describe(Token token)
+	{
+		switch (token)
+		{
+			case Token::eoln:   	return "<EOLN>";
+			case Token::undef:		return "<UNDEFINED>";
+			case Token::braceopen:	return "'('";
+			case Token::braceclose:	return "')'";
+
+			case Token::dot:		return "'.'";
+			case Token::comma:		return "','";
+			case Token::colon:		return "':'";
+			case Token::semicolon:	return "';'";
+			case Token::asterisk:	return "'*'";
+
+			case Token::eq_:		return "'='";
+			case Token::lt_:		return "'<'";
+			case Token::le_:		return "'<='";
+			case Token::gt_:		return "'>'";
+			case Token::ge_:		return "'>='";
+			case Token::ne_:		return "'<>'";
+
+			case Token::string:		return "string";
+			case Token::integer:	return "integer";
+			case Token::number:		return "number";
+			case Token::ident:		return "identifier";
+
+			case Token::select:		return "SELECT";
+			case Token::distinct:	return "DISTINCT";
+			case Token::from:		return "FROM";
+			case Token::update:		return "UPDATE";
+			case Token::set:		return "SET";
+			case Token::where:		return "WHERE";
+			case Token::and_:		return "AND";
+			case Token::or_:		return "OR";
+			case Token::not_:		return "NOT";
+			case Token::insert:		return "INSERT";
+			case Token::delete_:	return "DELETE";
+			case Token::into:		return "INTO";
+			case Token::values:		return "VALUES";
+
+			default:			assert(false); return "unknown token";
+		}
+	}
 
 	uint8_t GetNextByte();
 	unicode GetNextUnicode();
@@ -82,7 +215,15 @@ class Parser
 	void Retract();
 	void Match(Token token);
 
-	const cif::Datablock& mDb;
+	// parser rules
+	StatementPtr ParseStatement();
+	StatementPtr ParseSelect();
+	vector<string> ParseItemList();
+
+	cif::Condition ParseWhereClause(cif::Category& cat);
+	cif::Condition ParseNotWhereClause(cif::Category& cat);
+
+	cif::Datablock& mDb;
 	streambuf* mIs;
 	Token mLookahead;
 	stack<unicode> mBuffer;
@@ -159,7 +300,7 @@ unicode Parser::GetNextChar()
 		if (result >= 0x080)
 		{
 			if (result == 0x0ffff or result == 0x0fffe)
-				throw runtime_error("character " + to_hex(result) + " is not allowed");
+				throw runtime_error("character " + zeep::xml::to_hex(result) + " is not allowed");
 
 			// surrogate support
 			else if (result >= 0x0D800 and result <= 0x0DBFF)
@@ -213,6 +354,36 @@ void Parser::Retract()
 	mBuffer.push(zeep::xml::pop_last_char(mToken));
 }
 
+bool is_name_start_char(unicode uc)
+{
+	return
+		(uc >= L'A' and uc <= L'Z') or
+		uc == L'_' or
+		(uc >= L'a' and uc <= L'z') or
+		(uc >= 0x0C0 and uc <= 0x0D6) or
+		(uc >= 0x0D8 and uc <= 0x0F6) or
+		(uc >= 0x0F8 and uc <= 0x02FF) or
+		(uc >= 0x0370 and uc <= 0x037D) or
+		(uc >= 0x037F and uc <= 0x01FFF) or
+		(uc >= 0x0200C and uc <= 0x0200D) or
+		(uc >= 0x02070 and uc <= 0x0218F) or
+		(uc >= 0x02C00 and uc <= 0x02FEF) or
+		(uc >= 0x03001 and uc <= 0x0D7FF) or
+		(uc >= 0x0F900 and uc <= 0x0FDCF) or
+		(uc >= 0x0FDF0 and uc <= 0x0FFFD) or
+		(uc >= 0x010000 and uc <= 0x0EFFFF);	
+}
+
+bool is_name_char(unicode uc)
+{
+	return
+		(uc >= '0' and uc <= '9') or
+		uc == 0x0B7 or
+		is_name_start_char(uc) or
+		(uc >= 0x00300 and uc <= 0x0036F) or
+		(uc >= 0x0203F and uc <= 0x02040);
+}
+
 Parser::Token Parser::GetNextToken()
 {
 	enum class State
@@ -232,7 +403,10 @@ Parser::Token Parser::GetNextToken()
 		EscapeHex1,
 		EscapeHex2,
 		EscapeHex3,
-		EscapeHex4
+		EscapeHex4,
+
+		Less,
+		Greater
 	} state = State::Start;
 
 	Token token = Token::undef;
@@ -282,13 +456,22 @@ Parser::Token Parser::GetNextToken()
 			case '*':
 				token = Token::asterisk;
 				break;
+			case '=':
+				token = Token::eq_;
+				break;
+			case '<':
+				state = State::Less;
+				break;
+			case '>':
+				state = State::Greater;
+				break;
 			case ' ':
 			case '\n':
 			case '\r':
 			case '\t':
 				mToken.clear();
 				break;
-			case '"':
+			case '\'':
 				mToken.pop_back();
 				state = State::String;
 				break;
@@ -306,10 +489,32 @@ Parser::Token Parser::GetNextToken()
 					mTokenInteger = ch - '0';
 					state = State::Number;
 				}
-				else if (zeep::xml::is_name_start_char(ch))
+				else if (is_name_start_char(ch))
 					state = State::Literal;
 				else
-					throw runtime_error("invalid character (" + xml::to_hex(ch) + ") in command");
+					throw runtime_error("invalid character (" + zeep::xml::to_hex(ch) + "/'" + (isprint(ch) ? static_cast<char>(ch) : '.') + "') in command");
+			}
+			break;
+		
+		case State::Less:
+			if (ch == '=')
+				token = Token::le_;
+			else if (ch == '>')
+				token = Token::ne_;
+			else
+			{
+				Retract();
+				token = Token::lt_;
+			}
+			break;
+
+		case State::Greater:
+			if (ch == '=')
+				token = Token::ge_;
+			else
+			{
+				Retract();
+				token = Token::gt_;
 			}
 			break;
 
@@ -408,29 +613,29 @@ Parser::Token Parser::GetNextToken()
 			break;
 
 		case State::Literal:
-			if (not isalpha(ch))
+			if (not is_name_char(ch))
 			{
 				Retract();
 
-				if (iequals(mToken, "SELECT"))
-					token = Token::select;
-				else if (iequals(mToken, "FROM"))
-					token = Token::select;
-				else
-					token = Token::ident;
-				// if (mToken == "true")
-				// 	token = Token::True;
-				// else if (mToken == "false")
-				// 	token = Token::False;
-				// else if (mToken == "null")
-				// 	token = Token::Null;
-				// else
-				// 	throw runtime_error("Invalid literal found in command: " + mToken);
+					 if (iequals(mToken, "SELECT")) 	token = Token::select;
+				else if (iequals(mToken, "DISTINCT")) 	token = Token::distinct;
+				else if (iequals(mToken, "FROM")) 		token = Token::from;
+				else if (iequals(mToken, "UPDATE")) 	token = Token::update;
+				else if (iequals(mToken, "SET")) 		token = Token::set;
+				else if (iequals(mToken, "WHERE")) 		token = Token::where;
+				else if (iequals(mToken, "AND")) 		token = Token::and_;
+				else if (iequals(mToken, "OR")) 		token = Token::or_;
+				else if (iequals(mToken, "NOT")) 		token = Token::not_;
+				else if (iequals(mToken, "INSERT")) 	token = Token::insert;
+				else if (iequals(mToken, "DELETE")) 	token = Token::delete_;
+				else if (iequals(mToken, "INTO")) 		token = Token::into;
+				else if (iequals(mToken, "VALUES")) 	token = Token::values;
+				else									token = Token::ident;
 			}
 			break;
 		
 		case State::String:
-			if (ch == '\"')
+			if (ch == '\'')
 			{
 				token = Token::string;
 				mToken.pop_back();
@@ -447,7 +652,7 @@ Parser::Token Parser::GetNextToken()
 		case State::Escape:
 			switch (ch)
 			{
-				case '"':	
+				case '\'':	
 				case '\\':	
 				case '/':	
 					break;
@@ -519,7 +724,7 @@ Parser::Token Parser::GetNextToken()
 			else 
 				throw runtime_error("Invalid hex sequence in command");
 			mToken.pop_back();
-			append(mToken, hx);
+			zeep::xml::append(mToken, hx);
 			state = State::String;
 			break;
 		}
@@ -536,18 +741,214 @@ void Parser::Match(Token expected)
 	mLookahead = GetNextToken();
 }
 
-Statement* Parser::parse(const string& cmd)
+StatementPtr Parser::Parse(streambuf* is)
 {
-	mBuffer = cmd.begin();
-	mBufferEnd = cmd.end();
+	mIs = is;
 
-	GetNextToken();
-	Statement* result = nullptr;
+	mLookahead = GetNextToken();
+	shared_ptr<StatementList> result(new StatementList());
 
 	while (mLookahead != Token::eoln)
-		result = parse_statement(result);
+	{
+		auto stmt = ParseStatement();
+		result->Add(stmt);
+	}
 	
 	return result;
+}
+
+// -----------------------------------------------------------------------
+
+StatementPtr Parser::ParseStatement()
+{
+	StatementPtr result;
+
+	switch (mLookahead)
+	{
+		case Token::select:
+			Match(Token::select);
+			result = ParseSelect();
+			break;
+		
+		default:
+			// force error
+			Match(Token::select);
+	}
+	
+	Match(Token::semicolon);
+
+	return result;
+}
+
+// -----------------------------------------------------------------------
+
+StatementPtr Parser::ParseSelect()
+{
+	bool distinct = false;
+	if (mLookahead == Token::distinct)
+	{
+		distinct = true;
+		Match(Token::distinct);
+	}
+
+	auto items = ParseItemList();
+
+	Match(Token::from);
+
+	string cat = mToken;
+	Match(Token::ident);
+
+	auto category = mDb.get(cat);
+	if (category == nullptr)
+		throw runtime_error("Category " + cat + " is not defined in this file");
+
+	auto cv = category->getCatValidator();
+	if (cv != nullptr)
+	{
+		vector<string> nItems;
+
+		for (auto item: items)
+		{
+			if (item == "*")
+				transform(cv->mItemValidators.begin(), cv->mItemValidators.end(), back_inserter(nItems),
+					[cat](auto iv) { return iv.mTag; });
+			else
+				nItems.push_back(item);
+		}
+
+		swap(items, nItems);
+
+		items.erase(remove_if(items.begin(), items.end(), [category](auto item) { return not category->hasColumn(item); }), items.end());
+
+		for (auto item: items)
+		{
+			auto iv = cv->getValidatorForItem(item);
+			if (iv == nullptr)
+				throw runtime_error("Item " + item + " is not defined in the PDBx dictionary for category " + cat);
+		}	
+	}
+
+	if (mLookahead == Token::where)
+	{
+		Match(Token::where);
+		return StatementPtr{ new SelectStatement(*category, move(items), ParseNotWhereClause(*category)) };
+	}
+	else
+		return StatementPtr{ new SelectStatement(*category, move(items), cif::All()) };
+}
+
+// -----------------------------------------------------------------------
+
+vector<string> Parser::ParseItemList()
+{
+	vector<string> items;
+
+	for (;;)
+	{
+		if (mLookahead == Token::asterisk)
+		{
+			Match(Token::asterisk);
+			items.push_back("*");
+		}
+		else
+		{
+			items.push_back(mToken);
+			Match(Token::ident);
+		}
+
+		if (mLookahead == Token::comma)
+		{
+			Match(Token::comma);
+			continue;
+		}
+		
+		break;
+	}
+
+	return items;
+}
+
+// -----------------------------------------------------------------------
+
+cif::Condition Parser::ParseNotWhereClause(cif::Category& cat)
+{
+	cif::Condition result;
+
+	if (mLookahead == Token::not_)
+	{
+		Match(Token::not_);
+		result = cif::Not(ParseNotWhereClause(cat));
+	}
+	else if (mLookahead == Token::braceopen)
+	{
+		Match(Token::braceopen);
+		result = ParseNotWhereClause(cat);
+		Match(Token::braceclose);
+	}
+	else
+	{
+		result = ParseWhereClause(cat);
+
+		for (;;)
+		{
+			if (mLookahead == Token::and_)
+			{
+				Match(Token::and_);
+				result = move(result) and ParseNotWhereClause(cat);
+				continue;
+			}
+
+			if (mLookahead == Token::or_)
+			{
+				Match(Token::or_);
+				result = move(result) or ParseNotWhereClause(cat);
+				continue;
+			}
+
+			break;
+		}
+	}
+	return result;
+}
+
+// -----------------------------------------------------------------------
+
+cif::Condition Parser::ParseWhereClause(cif::Category& cat)
+{
+	string item = mToken;
+	Match(Token::ident);
+
+	if (mLookahead < Token::eq_ or mLookahead > Token::ne_)
+		Match(Token::eq_);
+	
+	auto oper = mLookahead;
+	Match(mLookahead);
+	
+	cif::Condition c;
+	string value = mToken;
+
+	switch (mLookahead)
+	{
+		case Token::integer:
+		case Token::number:
+		case Token::string:
+			Match(mLookahead);
+			break;
+		default:
+			Match(Token::string);
+	}
+	
+	switch (oper)
+	{
+		case Token::eq_:	return cif::Key(item) == value;
+		case Token::lt_:	return cif::Key(item) <  value;
+		case Token::le_:	return cif::Key(item) <= value;
+		case Token::gt_:	return cif::Key(item) >  value;
+		case Token::ge_:	return cif::Key(item) >= value;
+		case Token::ne_:	return cif::Key(item) != value;
+		default:			throw logic_error("should never happen");
+
+	}
 }
 
 }
@@ -598,27 +999,36 @@ int pr_main(int argc, char* argv[])
 	auto input = vm["input"].as<string>();
 	c::File file{fs::path(input)};
 
+	cql::Parser parser(file.data());
+
 	ifstream cmdFile;
 	if (vm.count("script"))
 	{
 		cmdFile.open(vm["script"].as<string>());
 		if (not cmdFile.is_open())
 			throw runtime_error("Failed to open command file " + vm["script"].as<string>());
-		cin.rdbuf(cmdFile.rdbuf());
+
+		auto stmt = parser.Parse(cmdFile.rdbuf());
+		if (stmt)
+			stmt->Execute();
 	}
-
-	cql::Parser parser(file.data());
-
-	string cmd;
-	while (getline(cin, cmd))
+	else
 	{
-		try
+		string cmd;
+		while (getline(cin, cmd))
 		{
-			auto stmt = parser.parse(cmd);
-		}
-		catch(const exception& e)
-		{
-			cerr << e.what() << endl;
+			try
+			{
+				istringstream is(cmd);
+
+				auto stmt = parser.Parse(is.rdbuf());
+				if (stmt)
+					stmt->Execute();
+			}
+			catch(const exception& e)
+			{
+				cerr << e.what() << endl;
+			}
 		}
 	}
 
