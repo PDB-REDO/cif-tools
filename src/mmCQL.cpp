1a29cfb8d3a0823200aace1e1956d6bb7ddb7ab8
Maarten L. Hekkelman <maarten@hekkelman.com>
2020-10-06 16:32:50 +0200

    remove zeep dependency

--- src/mmCQL.cpp
+++ src/mmCQL.cpp
@@ -35,7 +35,7 @@
 #include <boost/program_options.hpp>
 #include <boost/algorithm/string.hpp>
 
-#include <zeep/unicode-support.hpp>
+// #include <zeep/unicode-support.hpp>
 
 #include "cif++/Cif++.hpp"
 #include "cif++/Structure.hpp"
@@ -47,6 +47,144 @@ namespace ba = boost::algorithm;
 namespace fs = std::filesystem;
 // namespace io = boost::iostreams;
 namespace c = mmcif;
+
+using unicode = char32_t;
+
+namespace zeep {
+
+// inlines
+
+/// \brief Append a single unicode character to an utf-8 string
+inline void append(std::string& s, unicode uc)
+{
+	if (uc < 0x080)
+		s += (static_cast<char>(uc));
+	else if (uc < 0x0800)
+	{
+		char ch[2] = {
+			static_cast<char>(0x0c0 | (uc >> 6)),
+			static_cast<char>(0x080 | (uc & 0x3f))
+		};
+		s.append(ch, 2);
+	}
+	else if (uc < 0x00010000)
+	{
+		char ch[3] = {
+			static_cast<char>(0x0e0 | (uc >> 12)),
+			static_cast<char>(0x080 | ((uc >> 6) & 0x3f)),
+			static_cast<char>(0x080 | (uc & 0x3f))
+		};
+		s.append(ch, 3);
+	}
+	else
+	{
+		char ch[4] = {
+			static_cast<char>(0x0f0 | (uc >> 18)),
+			static_cast<char>(0x080 | ((uc >> 12) & 0x3f)),
+			static_cast<char>(0x080 | ((uc >> 6) & 0x3f)),
+			static_cast<char>(0x080 | (uc & 0x3f))
+		};
+		s.append(ch, 4);
+	}
+}
+
+/// \brief remove the last unicode character from an utf-8 string
+inline unicode pop_last_char(std::string& s)
+{
+	unicode result = 0;
+
+	if (not s.empty())
+	{
+		std::string::iterator ch = s.end() - 1;
+		
+		if ((*ch & 0x0080) == 0)
+		{
+			result = *ch;
+			s.erase(ch);
+		}
+		else
+		{
+			int o = 0;
+			
+			do
+			{
+				result |= (*ch & 0x03F) << o;
+				o += 6;
+				--ch;
+			}
+			while (ch != s.begin() and (*ch & 0x0C0) == 0x080);
+			
+			switch (o)
+			{
+				case  6: result |= (*ch & 0x01F) <<  6; break;
+				case 12: result |= (*ch & 0x00F) << 12; break;
+				case 18: result |= (*ch & 0x007) << 18; break;
+			}
+			
+			s.erase(ch, s.end());
+		}
+	}
+	
+	return result;
+}
+
+// this code only works if the input is valid utf-8
+/// \brief return the first unicode and the advanced pointer from a string
+template<typename Iter>
+std::tuple<unicode,Iter> get_first_char(Iter ptr)
+{
+	unicode result = static_cast<unsigned char>(*ptr);
+	++ptr;
+
+	if (result > 0x07f)
+	{
+		unsigned char ch[3];
+		
+		if ((result & 0x0E0) == 0x0C0)
+		{
+			ch[0] = static_cast<unsigned char>(*ptr); ++ptr;
+			result = ((result & 0x01F) << 6) | (ch[0] & 0x03F);
+		}
+		else if ((result & 0x0F0) == 0x0E0)
+		{
+			ch[0] = static_cast<unsigned char>(*ptr); ++ptr;
+			ch[1] = static_cast<unsigned char>(*ptr); ++ptr;
+			result = ((result & 0x00F) << 12) | ((ch[0] & 0x03F) << 6) | (ch[1] & 0x03F);
+		}
+		else if ((result & 0x0F8) == 0x0F0)
+		{
+			ch[0] = static_cast<unsigned char>(*ptr); ++ptr;
+			ch[1] = static_cast<unsigned char>(*ptr); ++ptr;
+			ch[2] = static_cast<unsigned char>(*ptr); ++ptr;
+			result = ((result & 0x007) << 18) | ((ch[0] & 0x03F) << 12) | ((ch[1] & 0x03F) << 6) | (ch[2] & 0x03F);
+		}
+	}
+
+	return std::make_tuple(result, ptr);
+}
+
+// --------------------------------------------------------------------
+
+inline std::string to_hex(uint32_t i)
+{
+	char s[sizeof(i) * 2 + 3];
+	char* p = s + sizeof(s);
+	*--p = 0;
+
+	const char kHexChars[] = "0123456789abcdef";
+
+	while (i)
+	{
+		*--p = kHexChars[i & 0x0F];
+		i >>= 4;
+	}
+
+	*--p = 'x';
+	*--p = '0';
+
+	return p;
+}	
+}
 
 // -----------------------------------------------------------------------
 
